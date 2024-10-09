78657b24566449fe39ce17108607b4af6279377f
WHORYZEN <kanishkshkl@gmail.com>
2024-10-10 00:33:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+    // Prevent the default form submission
+    event.preventDefault();
 
+    
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+
+    var fullName = firstName + ' ' + lastName;
+
+
+    alert(fullName);
 }
+
+
+document.getElementById('userForm').addEventListener('submit', getFormvalue);
